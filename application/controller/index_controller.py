4995f64cb521b833d5f09341b6519b0801645eb2
Guilherme <guilhermedms@hotmail.com>
2020-12-22 14:25:58 -0300

    Feat: json
    Foi feito a instanciação do json no controllador e passado para o HTML

--- application/controller/index_controller.py
+++ application/controller/index_controller.py
@@ -1,7 +1,7 @@
 from flask import render_template, request
 from application import app 
 import json
-
+from application.model.entity.produto import Produto
 
 
 @app.route('/')
@@ -10,8 +10,9 @@ def home():
   with open('C:\\Users\\PICHAU\\Desktop\\Faculdade 4\\Tassio\\P2\\Prova\\Linx-Impulse-test\\application\\view\\static\\json\\products.json') as product_file:
     product_list = json.load(product_file)
 
+  lista_produtos = []
   for product in product_list:
-    print(product['name'])
+    lista_produtos.append(Produto(product['id'], product['name'], product['image'],product['oldPrice'],product['price'],product['description'],product['installments']['count'],product['installments']['value']))
 
-  return render_template('index.html')
+  return render_template('index.html',  lista_produtos= lista_produtos)
     
